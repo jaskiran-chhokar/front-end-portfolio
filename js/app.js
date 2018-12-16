7ceef632ff25ff27170e424ca9deb9785f1c99f3
Jaskiran <jaskiranchhokar@gmail.com>
2018-12-16 14:40:41 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-
+let navbar = document.getElementById("navbar");
+window.onscroll = function () {
+	"use strict";
+	if (document.body.scrollTop >= 200) {
+		myNav.classList.add("navbar");
+	}
+	else {
+		myNav.classList.remove("navbar");
+	}
+};
